4bac3bede2c0c80360955dbd8a99536905d889b0
shariq-pro <shariq.ali@bitswits.com>
2022-11-07 12:32:05 +0500

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(){
+    this.message = 'Hello World';
+    console.log(this.message);
+    }
+    getUserName(){}
+    getUserStatus(){}
+    }
+    module.exports = User;
